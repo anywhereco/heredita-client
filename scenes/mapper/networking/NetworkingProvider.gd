342681bd6bcf665ae279817bac6618e49e474645
Whirling <infernity@infernity.dev>
2025-10-01 21:55:21 -0600

    incomplete: networking provider

---
+++ scenes/mapper/networking/NetworkingProvider.gd
@@ -0,0 +1,26 @@
+@abstract 
+class_name NetworkingProvider
+extends Resource
+
+enum NetworkingType {
+	LOCAL,
+	NETWORKED
+}
+
+@warning_ignore("unused_signal")
+signal message_recieved(from: int, message: String)
+
+@warning_ignore("unused_signal")
+signal pixels_recieved(from: int, message: String)
+
+@abstract
+func send_pixels(color: Color, pixels: Array[Vector2i]) -> void
+
+@abstract
+func send_message(message: String) -> void
+
+@abstract
+func get_locked_colors() -> Array[Color]
+
+@abstract
+func get_networking_type() -> NetworkingType
