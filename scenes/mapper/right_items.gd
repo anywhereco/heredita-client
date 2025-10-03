cb5ec98ffb69b15d7d810eddfa28c5d167b0ac72
Whirling <infernity@infernity.dev>
2025-10-02 21:10:52 -0600

    make chat scale within reasonable bounds on open

---
+++ scenes/mapper/right_items.gd
@@ -0,0 +1,16 @@
+extends VBoxContainer
+
+var have_processed: bool = false
+
+const MAX_DEFAULT_CHAT_WIDTH := 225
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	if not have_processed:
+		print(size)
+		if size.x >= MAX_DEFAULT_CHAT_WIDTH:
+			size_flags_stretch_ratio = (MAX_DEFAULT_CHAT_WIDTH / size.x)
+		have_processed = true
+	# TODO: maybe always change on resize? user can still manually resize the chat bar if they want anyway
