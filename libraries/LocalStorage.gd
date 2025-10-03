d50df9bca17a479ed35ca1b3d94c40d4216eabd0
Whirling <infernity@infernity.dev>
2025-10-03 14:36:59 -0600

    fix: type localstorage

--- libraries/LocalStorage.gd
+++ libraries/LocalStorage.gd
@@ -1,7 +1,7 @@
 extends Node
 
-var _localStorage = JavaScriptBridge.get_interface("localStorage") if OS.has_feature("web") else null
-var _tempStorage = {}
+var _localStorage := JavaScriptBridge.get_interface("localStorage") if OS.has_feature("web") else null
+var _tempStorage := {}
 const _save_path = "user://token_DO_NOT_SHARE.save" # Technically doesn't just store the token but I think that
 													# this is better since it conveys more danger
  
@@ -14,24 +14,22 @@ func _init() -> void: # We use _init to load stuff *first* before any ready call
 	if not OS.has_feature("web"):
 		_load_data()
 
-func _notification(notif: int):
-	
+func _notification(notif: int) -> void:
 	if notif == NOTIFICATION_APPLICATION_PAUSED: # Mobile exclusive
 		_save_data() # The app could be killed without warning so we save the data in advance
-		
 	if notif == NOTIFICATION_WM_CLOSE_REQUEST:
 		if not OS.has_feature("web"): # Save data if not on web
 			_save_data()
 		get_tree().quit()
 
-func _save_data():
-	var file = FileAccess.open(_save_path, FileAccess.WRITE)
+func _save_data() -> void: 
+	var file := FileAccess.open(_save_path, FileAccess.WRITE)
 	file.store_var(_tempStorage)
 
-func _load_data():
+func _load_data() -> void:
 	if FileAccess.file_exists(_save_path):
-		var file = FileAccess.open(_save_path, FileAccess.READ)
-		var storage = file.get_var()
+		var file := FileAccess.open(_save_path, FileAccess.READ)
+		var storage: Variant = file.get_var()
 		if storage != null:
 			_tempStorage = storage
 
@@ -43,7 +41,7 @@ func set_item(key: String, value: String) -> LocalStorageResult:
 	return LocalStorageResult.SUCCESSFUL
 
 func get_item(key: String) -> Result:
-	var item
+	var item: Variant
 	if not OS.has_feature("web"):
 		item = _tempStorage.get(key)
 	else:
@@ -53,7 +51,7 @@ func get_item(key: String) -> Result:
 	return Result.err(LocalStorageResult.KEY_DOESNT_EXIST)
 
 func get_item_or_fallback(key: String, fallback: Variant = null) -> Variant:
-	var item
+	var item: Variant
 	if not OS.has_feature("web"):
 		item = _tempStorage.get(key)
 	else:
