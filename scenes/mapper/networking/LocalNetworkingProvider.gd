670755938839ee4788846e30eb006cbc7e580681
diginist <xanmangaming8@gmail.com>
2025-10-19 15:21:35 -0500

    incredibly small accomplishment

---
+++ scenes/mapper/networking/LocalNetworkingProvider.gd
@@ -0,0 +1,14 @@
+@abstract 
+extends NetworkingProvider
+
+@abstract
+func send_pixels(color: Color, pixels: Array[Vector2i]) -> void
+
+func send_message(message: String) -> void:
+	pass #locate server, send to server
+
+@abstract
+func get_locked_colors() -> Array[Color]
+
+@abstract
+func get_networking_type() -> NetworkingType
