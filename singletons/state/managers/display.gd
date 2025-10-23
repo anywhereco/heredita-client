edd703ff58b9cb150daa5297a4c4ac523b158544
Whirling <infernity@infernity.dev>
2025-10-23 00:01:05 -0600

    feat: 4 of them

--- singletons/state/managers/display.gd
+++ singletons/state/managers/display.gd
@@ -22,11 +22,24 @@ const ROOT_WINDOW_SCRIPT: Script = preload("res://singletons/state/window_notifi
 
 ## The scale relative to the default viewport size after scale adjustment.
 ## Warning: Do not use this scale for most things; only use it if you need an assured size for something nonessential.
-@onready var stretch_scale := ReactiveFloat.new(_calculate_stretch_scale())
+@onready var stretch_scale := ReactiveFloat.new(1)
+
+## The scale relative to the default viewport size after scale adjustment, on the axis which is smaller.
+## Warning: Do not use this scale for most things; only use it if you need an assured size for something nonessential.
+@onready var reduced_stretch_scale := ReactiveFloat.new(1)
+
+## The scale relative to the default viewport size after scale adjustment, on the X axis.
+## Warning: Do not use this scale for most things; only use it if you need an assured size for something nonessential.
+@onready var stretch_scale_x := ReactiveFloat.new(1)
+
+## The scale relative to the default viewport size after scale adjustment, on the Y axis.
+## Warning: Do not use this scale for most things; only use it if you need an assured size for something nonessential.
+@onready var stretch_scale_y := ReactiveFloat.new(1)
 
 func _ready() -> void:
 	root_window.set_script(ROOT_WINDOW_SCRIPT)
 	root_window.content_scale_factor = scale.value
+	_calculate_stretch_scales()
 
 func _calculate_true_scale() -> float:
 	var adjusted_resolution := root_window.size / display_server_recommended_scale.value
@@ -39,13 +52,16 @@ func _calculate_true_scale() -> float:
 		return temp_scale
 	return display_server_recommended_scale.value
 
-func _calculate_stretch_scale() -> float:
+func _calculate_stretch_scales() -> void:
 	var adjusted_resolution := root_window.size / scale.value
 	
 	var scale_x := adjusted_resolution.x / DEFAULT_VIEWPORT_SIZE.x
 	var scale_y := adjusted_resolution.y / DEFAULT_VIEWPORT_SIZE.y
 	
-	return scale_x if scale_x > scale_y else scale_y
+	stretch_scale.value = maxf(scale_x, scale_y)
+	reduced_stretch_scale.value = minf(scale_x, scale_y)
+	stretch_scale_x.value = scale_x
+	stretch_scale_y.value = scale_y
 
 func _root_window_notifications(notif: int) -> void:
 	match notif:
@@ -58,5 +74,5 @@ func _root_window_rescaled_or_moved() -> void:
 	display_server_recommended_scale.value = DisplayServer.screen_get_scale(DisplayServer.SCREEN_OF_MAIN_WINDOW) 
 	if prev_scale != scale.value:
 		root_window.content_scale_factor = scale.value
-	stretch_scale.value = _calculate_stretch_scale()
+	_calculate_stretch_scales()
 	root_window_moved_or_resized.emit()
