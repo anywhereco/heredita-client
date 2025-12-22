9b908ad6d2899c3181a6fb6344236062dd4dd84c
Whirling <infernity@infernity.dev>
2025-12-22 13:23:53 -0700

    3d

--- scenes/lobby/foreground/temporary_mapper_opener.gd
+++ scenes/lobby/foreground/temporary_mapper_opener.gd
@@ -1,4 +1,4 @@
 extends Button
 
 func _pressed() -> void:
-	get_tree().change_scene_to_packed(preload("res://scenes/mapper/mapper.tscn"))
+	get_tree().change_scene_to_packed(preload("res://scenes/mapper/mapper3d.tscn"))
---
+++ scenes/mapper/mapper_3d.gd
@@ -0,0 +1,26 @@
+class_name MapperRoot
+extends Node3D
+
+enum Tool {
+	BRUSH,
+	FILL,
+	LINE,
+	PICK,
+	INSPECT,
+	PAN,
+	MARKING,
+	SELECT
+}
+
+var tool := ReactiveInt.new(Tool.BRUSH)
+
+var base_zoom := ReactiveFloat.new(1)
+var effective_zoom := ReactiveFloat.new(1)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
