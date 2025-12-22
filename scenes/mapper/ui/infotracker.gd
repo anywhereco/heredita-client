25ac45bde216922b10c74576840798f90b68c07f
Whirling <infernity@infernity.dev>
2025-12-22 14:49:44 -0700

    fps counter

--- scenes/mapper/ui/infotracker.gd
+++ scenes/mapper/ui/infotracker.gd
@@ -2,9 +2,9 @@ extends Label
 
 @onready var mapper: MapperRoot = $"../../../../../.."
 
-# Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-#	mapper.base_zoom.value_changed.connect(_zoom)
-
-#func _zoom(val: ReactiveFloat) -> void:
-#	text = "zoom: %d%%" % (val.value * 100)
+func _ready() -> void:
+	pass
+	
+func _process(delta: float) -> void:
+	text = "\n"
+	text += "fps: %s\n" % Engine.get_frames_per_second()w
