71b698fec019b295716a32e37bbd01c795037685
diginist <xanmanxan@outlook.com>
2025-12-22 12:15:05 -0600

    old server changes

--- scenes/mapper/networking/LocalServer.gd
+++ scenes/mapper/networking/LocalServer.gd
@@ -1,2 +1,8 @@
 extends Server
 class_name LocalServer
+
+func get_requests() -> void: #Receive requests and, if applicable, pass them to the right room
+	pass
+
+func send_request() -> void:
+	pass
