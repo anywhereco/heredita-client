b6c51d628d15eceda7e1f3a41ad9fdffa153490f
Whirling <infernity@infernity.dev>
2025-12-22 13:38:08 -0700

    player movement

--- scenes/mapper/networking/NetworkingProvider.gd
+++ scenes/mapper/networking/NetworkingProvider.gd
@@ -13,11 +13,17 @@ signal message_recieved(from: int, message: String)
 @warning_ignore("unused_signal")
 signal pixels_recieved(from: int, color: Color, pixels: Array[Vector2i])
 
+@warning_ignore("unused_signal")
+signal player_moved(from: int, location: Vector3)
+
 @abstract
 func send_pixels(color: Color, pixels: Array[Vector2i]) -> void
 
 @abstract
 func send_message(message: String) -> void
+
+@abstract
+func move_player(to: Vector3) -> void
 
 @abstract
 func get_locked_colors() -> Array[Color]
