69f9cf5984c83b526ab66270ce85704439e415e7
Whirling <infernity@infernity.dev>
2025-12-22 15:34:35 -0700

    fix stuttering

--- scenes/mapper/camera_pivot.gd
+++ scenes/mapper/camera_pivot.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	var wanted_pos := player.position
 	wanted_pos.y += .5
 	position += (wanted_pos - position) * (1 - exp(-40*delta))
