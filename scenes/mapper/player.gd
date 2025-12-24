1695603f9688435d82fd4b91ba59e676642e83a2
Whirling <infernity@infernity.dev>
2025-12-24 14:14:44 -0700

    bunnyhopping

--- scenes/mapper/player.gd
+++ scenes/mapper/player.gd
@@ -6,6 +6,8 @@ const SPEED = 5.0
 const SPRINT_MULT = 3.0
 const JUMP_VELOCITY = 4.5
 
+var was_on_floor_last_frame: bool = false
+
 func setup_velocity(direction: Vector3, delta: float) -> void:
 	var target_vel := direction * SPEED * (SPRINT_MULT if Input.is_key_pressed(KEY_SHIFT) else 1.0)
 	var clock: float
@@ -36,7 +38,8 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if (Input.is_action_just_pressed("jump") and is_on_floor()) or \
+	(Input.is_action_pressed("jump") and is_on_floor() and not was_on_floor_last_frame):
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
