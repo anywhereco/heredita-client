fda61e5d690eee78f4dc95d3952f4e66d1836cb3
Whirling <infernity@infernity.dev>
2025-12-24 21:03:08 -0700

    overlay pt1

--- scenes/mapper/mapper_3d.gd
+++ scenes/mapper/mapper_3d.gd
@@ -2,6 +2,7 @@ class_name MapperRoot
 extends Node3D
 
 enum Tool {
+	NONE,
 	BRUSH,
 	FILL,
 	LINE,
@@ -14,7 +15,7 @@ enum Tool {
 
 static var _instance: MapperRoot
 
-var tool := ReactiveInt.new(Tool.BRUSH)
+var tool := ReactiveInt.new(Tool.NONE)
 
 func _init() -> void:
 	_instance = self
@@ -26,3 +27,14 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+func process_tool_use(event: InputEvent, map: Map) -> void:
+	match tool.value:
+		Tool.NONE:
+			return
+		Tool.BRUSH:
+			if event.is_action_pressed("pick_paint"):
+				pass
+		_:
+			push_error("Tool \"%s\" doesn't have code to handle events!" % MapperRoot.Tool.keys()[tool.value].to_lower())
+			return
