a56d3e810c9d67a4c173da4b5a162a853d1e05bd
Whirling <infernity@infernity.dev>
2026-01-02 17:24:45 -0700

    topbar functionality

--- scenes/mapper/ui/color_rgb.gd
+++ scenes/mapper/ui/color_rgb.gd
@@ -8,13 +8,16 @@ func _value_changed(new_color: ReactiveColor) -> void:
 	$B.set_value_no_signal(new_color.value.b*255)
 	
 func _r_changed(value: float) -> void:
+	@warning_ignore("unsafe_call_argument")
 	var color: Color = Color(value/255, $G.value/255, $B.value/255)
 	CPicker.color.value = color
 
 func _g_changed(value: float) -> void:
+	@warning_ignore("unsafe_call_argument")
 	var color: Color = Color($R.value/255, value/255, $B.value/255)
 	CPicker.color.value = color
 
 func _b_changed(value: float) -> void:
+	@warning_ignore("unsafe_call_argument")
 	var color: Color = Color($R.value/255, $G.value/255, value/255)
 	CPicker.color.value = color
