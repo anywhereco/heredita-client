310335e1e009db5418708b6e7aed0de984d57107
Whirling <infernity@infernity.dev>
2026-01-18 15:55:49 -0700

    Worlds Hardest Fix

--- scenes/main_menu/background/map_scaler.gd
+++ scenes/main_menu/background/map_scaler.gd
@@ -2,6 +2,7 @@ extends Sprite2D
 
 func _ready() -> void:
 	State.display.stretch_scale.value_changed.connect(rescale)
+	rescale(State.display.stretch_scale)
 
 func rescale(_scale: ReactiveFloat) -> void:
 	scale = Vector2(_scale.value, _scale.value)
