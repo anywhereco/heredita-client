f90b3fa1a442bc383dc591e8ef1a37993d28bec4
Whirling <infernity@infernity.dev>
2026-02-19 22:20:46 -0700

    update url

--- singletons/state/Static.gd
+++ singletons/state/Static.gd
@@ -3,6 +3,6 @@ class_name Statics
 extends Object
 
 #const HEREDITA_URL := "http://localhost:9000"
-const HEREDITA_URL := "https://demo.heredita.net" 
+const HEREDITA_URL := "https://app.heredita.net" 
 
 const LOGIN_URL := "login.infernity.dev"
