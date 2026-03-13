bc1e674d7d0614e0ad073be8b2a7467402d5cc90
Whirling <infernity@infernity.dev>
2026-03-12 20:27:31 -0600

    Evil Toast Deletor

--- addons/toastparty/toastparty.gd
+++
@@ -1,9 +0,0 @@
-@tool
-extends EditorPlugin
-
-const AUTOLOAD_NAME = "ToastParty"
-func _enter_tree():
-	add_autoload_singleton(AUTOLOAD_NAME, "res://addons/toastparty/toast-autoload.gd")
-
-func _exit_tree():
-	remove_autoload_singleton(AUTOLOAD_NAME)
