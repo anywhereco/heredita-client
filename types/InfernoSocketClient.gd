40bc7e153695f93971ab79d26b7839bfe5256c59
diginist <xanmanxan@outlook.com>
2026-03-27 20:48:44 -0500

    server weird bugs fix

--- types/InfernoSocketClient.gd
+++ types/InfernoSocketClient.gd
@@ -212,6 +212,7 @@ func _poll_string(message: Dictionary) -> void:
 			"_is2_handshake":
 				print("handshake")
 				if creating_room:
+					send("_is2_create_room", creating_room)
 					chunk_sender.send(ISUtil.BinaryEvents.SYNC_MAP, -1, creating_map)
 					return
 					#var map_compr := creating_map.compress(FileAccess.COMPRESSION_FASTLZ)
