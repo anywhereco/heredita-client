746d8e7bb574a2737e713d7f40efbdf18d94900b
Whirling <infernity@infernity.dev>
2026-05-06 22:15:00 -0600

    move room to typing

---
+++ libraries/typing/Room.gd
@@ -0,0 +1,47 @@
+class_name Room
+
+var name := "Unnamed Room"
+var description := ""
+var player_limit := 8
+var password := ""
+
+var map: MapData
+
+# ReactiveDictionary[ServerUser]
+var players: ReactiveDictionary = ReactiveDictionary.new({})
+var player_ids_chronological: Array[int] = []
+var id_iterator := 0  #next id for use
+var close_on_empty := true
+
+var banned_ips := []
+
+
+func taken_usernames() -> Array[String]:
+	var l: Array[String] = []
+	for player_id: int in players.keys():
+		if players.getv(player_id).logged_in:
+			l.append(players.getv(player_id).username)
+	return l
+
+
+func player_info() -> Dictionary:
+	var l := {}
+	for player_id: int in players.keys():
+		l[player_id] = {
+			"username": players.getv(player_id).username,
+			"logged_in": players.getv(player_id).logged_in,
+			"profile": players.getv(player_id).profile,
+			"operator": players.getv(player_id).operator,
+			"rank": players.getv(player_id).rank
+		}
+	return l
+
+
+func to_json() -> Dictionary:
+	return {
+		"name": name,
+		"description": description,
+		"player_count": players.size(),
+		"player_limit": player_limit,
+		"password_protected": not password.is_empty()
+	}
