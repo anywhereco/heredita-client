ea07513499d2e116395b3dff9473cd91b6d6dc92
Whirling <infernity@infernity.dev>
2026-07-04 14:44:34 -0600

    add hand cursor

--- libraries/autoloading/cursor/VirtualMouse.gd
+++ libraries/autoloading/cursor/VirtualMouse.gd
@@ -1,14 +1,15 @@
 class_name VirtualMouse
 extends Sprite2D
 
-enum Action { DEFAULT, BRUSH, DICE, PANNING }
+enum Action { DEFAULT, BRUSH, DICE, PANNING, HAND }
 
 const ACTION_TEX_MAP: Dictionary[Action, Texture2D] = {
 	Action.DEFAULT: preload("uid://dmqeubohwubs"),
 	Action.BRUSH: preload("uid://c5cl8n3npd8bn"),
 	Action.DICE: preload("uid://dmqeubohwubs"),  # TODO make better dice cursor [for now we default to regular cursor]
 	#Action.DICE: preload("uid://dumkbg3oyffeh"),
-	Action.PANNING: preload("uid://byfqo6a6v6gvn")
+	Action.PANNING: preload("uid://byfqo6a6v6gvn"),
+	Action.HAND: preload("uid://btcu27i6g5qag")
 }
 
 const ACTION_OFFSET_MAP: Dictionary[Action, Vector2] = {
@@ -16,11 +17,12 @@ const ACTION_OFFSET_MAP: Dictionary[Action, Vector2] = {
 	Action.BRUSH: Vector2(12, 12),
 	Action.DICE: Vector2(7, 9),
 	#Action.DICE: Vector2(0, 0),
-	Action.PANNING: Vector2(0, 0)
+	Action.PANNING: Vector2(0, 0),
+	Action.HAND: Vector2(4, 13),
 }
 
 static var _instance: VirtualMouse
-
+											
 var action: Action = Action.DEFAULT
 var tool_action: Action = Action.DEFAULT
 
@@ -29,6 +31,10 @@ func _init() -> void:
 	_instance = self
 
 func _ready() -> void:
+	Input.set_custom_mouse_cursor(
+		ACTION_TEX_MAP[Action.HAND],
+		Input.CursorShape.CURSOR_POINTING_HAND,
+		ACTION_TEX_MAP[Action.HAND].get_size()/2-ACTION_OFFSET_MAP[Action.HAND])
 	set_action(Action.DEFAULT)
 
 func set_action(new_action: Action) -> void:
