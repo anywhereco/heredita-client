ab014a38aa6bbb7ddbdb1f31ea7b3d3ec73f2b86
diginist <xanmangaming8@gmail.com>
2026-08-15 18:14:40 -0500

    add file prompter utility library

--- scenes/mapper/ui/chat_save_log.gd
+++ scenes/mapper/ui/chat_save_log.gd
@@ -3,10 +3,6 @@ extends Button
 @onready var chat := get_parent().get_parent().get_parent().get_parent()
 
 func _pressed() -> void:
-	var log: String = chat.messages_text_full
-	var buffer := log.to_utf8_buffer()
-	if OS.has_feature("web"):
-		JavaScriptBridge.download_buffer(buffer, "chat_log.txt", "text/plain")
-	else:
-		var file := FileAccess.open("user://chat_log.txt", FileAccess.WRITE)
-		file.store_buffer(buffer)
+	var chat_log: String = chat.messages_text_full
+	var buffer := chat_log.to_utf8_buffer()
+	FilePrompter.save(self, buffer, "chat_log.txt", "chat_log")
--- scenes/mapper/ui/export_button.gd
+++ scenes/mapper/ui/export_button.gd
@@ -3,8 +3,5 @@ extends Button
 
 func _pressed() -> void:
 	var img: Image = Map._instance.get_map_as_image()
-	if OS.has_feature("web"):
-		var buffer := img.save_png_to_buffer()
-		JavaScriptBridge.download_buffer(buffer, "saved_map.png", "image/png")
-	else:
-		img.save_png("user://saved_map.png")
+	var buffer := img.save_png_to_buffer()
+	FilePrompter.save(self, buffer, "saved_map.png", "map_export", "image/png")
